094883cfd48c744717354090e1b80673df45ad3b
liamoctavio <ocmolina47@gmail.com>
2024-08-17 11:07:53 -0400

    Subida Inicial

---
+++ src/main/java/com/peliculas/peliculas_service/PeliculasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peliculas.peliculas_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasServiceApplication.class, args);
+	}
+
+}
